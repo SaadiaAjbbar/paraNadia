2b367181d12786d7ec2fd83cae7b902dc4cec909
SaadiaAjbbar <ajbbarsaadia6@gmail.com>
2026-09-17 08:07:01 +0100

    fix: add temporary route for db migration

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Artisan;
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/init-db', function () {
+    Artisan::call('migrate:fresh', ['--seed' => true, '--force' => true]);
+    return 'Database migrated and seeded successfully!';
 });
